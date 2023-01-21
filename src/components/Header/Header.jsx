351956d1f6101ccf82f0b48a20ab0e67929d9889
leandro castro <leandro.713@hotmail.com>
2023-01-21 13:36:54 -0300

    header 21.01

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-import "./header.css";
+import "./Header.css";
+import DownloadImg from "../img/download-silver.png";
 
 export default function Header(){
     return <>
-    
+    <div className="header">
+        <div className="logo">
+            <h4>meu logo</h4>
+        </div>
+        <div className="menu">
+            <ul className="menu-list">
+                <a link="#" target="_blank">
+                    <li>Knowledges</li>
+                </a>
+                <a link="#" target="_blank">
+                    <li>Skills</li>
+                </a>
+                <a link="#" target="_blank">
+                    <li>About</li>
+                </a>
+                <a link="#" target="_blank">
+                    <li>Contact</li>
+                </a>
+            </ul>
+        </div>
+        <div className="curriculum">
+            <button className="download">
+                Curriculum
+                <img src={DownloadImg} /> 
+                
+            </button>
+        </div>
+    </div>
     </>
 } 
