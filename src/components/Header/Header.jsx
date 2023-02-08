d4a150668029ab0599e72519407038c5fbedfbe7
leandro castro <leandro.713@hotmail.com>
2023-02-08 09:30:16 -0300

    08.02.2023

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
+import React from "react";
 import "./Header.css";
 import DownloadImg from "../img/download-silver.png";
+import flagBRA from "../img/flag-bra.png";
+import flagUSA from "../img/flag-usa.png";
 
-export default function Header(){
-    return <>
-    <div className="header">
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        
         <div className="logo">
-            <h4>meu logo</h4>
+          <h4>my logo</h4>
         </div>
+        
         <div className="menu">
-            <ul className="menu-list">
-                <a link="#" target="_blank">
-                    <li>Knowledges</li>
-                </a>
-                <a link="#" target="_blank">
-                    <li>Skills</li>
-                </a>
-                <a link="#" target="_blank">
-                    <li>About</li>
-                </a>
-                <a link="#" target="_blank">
-                    <li>Contact</li>
-                </a>
-            </ul>
+          <ul className="menu-list">
+            <a link="#" target="_blank">
+              <li>Knowledges</li>
+            </a>
+            <a link="#" target="_blank">
+              <li>Skills</li>
+            </a>
+            <a link="#" target="_blank">
+              <li>About</li>
+            </a>
+            <a link="#" target="_blank">
+              <li>Contact</li>
+            </a>
+          </ul>
         </div>
+        
+        <div className="flags">
+          <img src={flagBRA} alt="" className="flag" id="flag-bra" />
+          <img src={flagUSA} alt="" className="flag" id="flag-usa" />
+        </div>
+        
         <div className="curriculum">
-            <button className="download">
-                Curriculum
-                <img src={DownloadImg} /> 
-                
-            </button>
+          <button className="download">
+            Curriculum
+            <img src={DownloadImg} />
+          </button>
         </div>
-    </div>
+      
+      </div>
     </>
-} 
+  );
+}
