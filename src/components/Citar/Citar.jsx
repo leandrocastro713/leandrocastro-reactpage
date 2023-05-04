c79629707fc67593c92b4fb55173cdb010388a3b
leandro castro <leandro.713@hotmail.com>
2023-05-04 01:34:37 -0300

    04.05.2023 B

--- src/components/Citar/Citar.jsx
+++ src/components/Citar/Citar.jsx
@@ -4,7 +4,7 @@ import "./Citar.css";
 export default function Citar() {
   return (
     <section className="content">
-      <p className="frase">"Quem trabalha com o que ama está sempre de férias.</p>
+      <p className="frase">"Quem trabalha com o que ama está sempre de férias."</p>
       <p className="autor"><i>Autor desconhecido</i></p>
     </section>
   );
