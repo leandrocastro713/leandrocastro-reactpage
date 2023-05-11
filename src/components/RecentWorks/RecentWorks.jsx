791085f65f212d8c67826a79c20eac4968bf06d5
leandro castro <leandro.713@hotmail.com>
2023-05-11 18:30:31 -0300

    11.05 maisX

--- src/components/RecentWorks/RecentWorks.jsx
+++ src/components/RecentWorks/RecentWorks.jsx
@@ -1,54 +1,101 @@
-import React from "react"
-import "./RecentWorks.css"
+import React from "react";
+import "./RecentWorks.css";
 
-export default function RecentWorks(){
-  return(
+export default function RecentWorks() {
+  return (
     <section className="recent-works">
-      <div class="recent-thumbs">
+      <div className="recent-thumbs">
         <div className="recent-thumbs-block">
-          <img src={require("./thumb-bar1.jpg")} alt=""/>
+          <img src={require("./thumb-bar1.jpg")} alt="" />
           <p>Social Media</p>
         </div>
         <div className="recent-thumbs-block">
-            <img src={require("./thumb-bar2.jpg")} alt=""/>
-            <p>Social Media</p>
+          <img src={require("./thumb-bar2.jpg")} alt="" />
+          <p>Social Media</p>
         </div>
         <div className="recent-thumbs-block">
-            <img src={require("./thumb-bruno.jpg")} alt=""/>
-            <p>Social Media</p>
+          <img src={require("./thumb-bruno.jpg")} alt="" />
+          <p>Social Media</p>
         </div>
         <div className="recent-thumbs-block">
-            <img src={require("./thumb-center.jpg")} alt=""/>
-            <p>Social Media</p>
+          <img src={require("./thumb-center.jpg")} alt="" />
+          <p>Social Media</p>
         </div>
         <div className="recent-thumbs-block">
-            <img src={require("./thumb-giovanni.jpg")} alt=""/>
-            <p>Social Media</p>
+          <img src={require("./thumb-giovanni.jpg")} alt="" />
+          <p>Social Media</p>
         </div>
+
         <div className="recent-thumbs-block">
-            <img src={require("./thumb-joyce-cartão.jpg")} alt=""/>
+          <a href="../RecentWorks/thumb-joyce-cartão.jpg">
+            <img
+              src={require("../RecentWorks/thumb-joyce-cartão.jpg")}
+              alt=""
+            />
             <p>Social Media</p>
+          </a>
         </div>
-      
+
         <div className="recent-thumbs-block">
-            <a href="https://joycesilvanocabeloslivres.vercel.app/" target="_blank" rel="noopener noreferrer">
-            <img src={require("./thumb-joyce.jpg")} alt=""/>
+          <a
+            href="https://joycesilvanocabeloslivres.vercel.app/"
+            target="_blank"
+          >
+            <img src={require("./thumb-joyce.jpg")} alt="" />
             <p>Site</p>
-            </a>
+          </a>
         </div>
+
         <div className="recent-thumbs-block">
-            <img src={require("./thumb-stop.jpg")} alt=""/>
-            <p>Social Media</p>
+          <img src={require("./thumb-stop.jpg")} alt="" />
+          <p>Social Media</p>
         </div>
-          
-    </div>
+      </div>
+      <div className="mais-link-banner">
+        <a id="mais-link" href="./mais.html" target="_blank">
+          <p id="mais">Mais ...</p>
+        </a>
+      </div>
+      {/*------------------- MAIS X CONTAINER ------------------- */}
+      <div className="maisx-container">
+        <a href="./img2/card-bayern-vs-barcelona.jpg" target="_blank">
+            <img src={require("../img2/card-bayern-vs-barcelona.jpg")} alt=""/>
+        </a>
+        <a href="./img2/card-bruno.jpg" target="_blank">
+            <img src={require("../img2/card-bruno.jpg")} alt=""/>
+        </a>
+        <a href="./img2/card-cleber.jpg" target="_blank">
+            <img src={require("../img2/card-cleber.jpg")} alt=""/>
+        </a>
+        <a href="./img2/card-imperio.jpg" target="_blank">
+            <img src="./img2/card-imperio.jpg" alt=""/>
+        </a>
+        <a href="./img2/card-jerry.jpg" target="_blank">
+            <img src="./img2/card-jerry.jpg" alt=""/>
+        </a>
+        <a href="./img2/card-lise.jpg" target="_blank">
+            <img src="./img2/card-lise.jpg" alt=""/>
+        </a>
+        <a href="./img2/card-marilia.jpg" target="_blank">
+            <img src="./img2/card-marilia.jpg" alt=""/>
+        </a>
+        <a href="./img2/card-real-vs-atletico.jpg" target="_blank">
+            <img src="./img2/card-real-vs-atletico.jpg" alt=""/>
+        </a>
+        <a href="./img2/card-rizzi.jpg" target="_blank">
+            <img src="./img2/card-rizzi.jpg" alt=""/>
+        </a>
+        <a href="./img2/cartao.jpg" target="_blank">
+            <img src="./img2/cartao.jpg" alt=""/>
+        </a>
+        <a href="./img2/flyer-joyce.jpg" target="_blank">
+            <img src="./img2/flyer-joyce.jpg" alt=""/>
+        </a>
+        <a href="./img2/flyer-stop-xis-calabresa.jpg" Target="_blank">
+            <img src="./img2/flyer-stop-xis-calabresa.jpg" alt=""/>
+        </a>
 
-    <a href="./mais.html" target="_blank">
-        <p id = "mais">Mais ...</p>
-    </a> 
-
+      </div>
     </section>
-
-  )
-
+  );
 }
