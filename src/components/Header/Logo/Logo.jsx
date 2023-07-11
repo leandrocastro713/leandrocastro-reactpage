37637734025776597ecebff7391db0fd3ddf437e
leandro castro <leandro.713@hotmail.com>
2023-07-11 15:09:43 -0300

    ajustando o skills gauges em 320px

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Logo.css"
+import useState from 'react'
 
 export default function Logo({ ptbr }) {
-    // console.log(lang)
+    
+    let msgtxt = ''
+
+    if(ptbr=='pt'){
+        msgtxt = 'Aceitando sugestões de logotipo :)'
+    }else {
+        msgtxt = 'Accepting logo suggestions :)'
+    }
+
+    function showMessageBox(){
+        alert(msgtxt)
+    }
+
     return(
-        <div className="Logo">
+        <div className="logo" onClick = {showMessageBox} >
             { ptbr=='pt' && <h4>meu logo</h4> }
             { ptbr=='en' && <h4>my logo</h4> }
         </div>
